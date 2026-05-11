fd17b62412a07b1de0c5a5097b0d35ffb8755a54
bikbakboo <ben1641979@gmail.com>
2026-05-11 21:15:36 +0200

    Update FilmpalastProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/FilmpalastProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/FilmpalastProvider.kt
@@ -1,7 +1,8 @@
 package com.example
 
-import com.lagradost.cloudstream3.*
-import org.jsoup.Jsoup
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.SearchResponse
 
 class FilmpalastProvider : MainAPI() {
     override var mainUrl = "https://filmpalast.to"
@@ -9,11 +10,6 @@ class FilmpalastProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.Movie)
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val document = app.get("$mainUrl/search/story/$query").document
-        return document.select("article.item").mapNotNull {
-            val title = it.selectFirst("h2")?.text() ?: return@mapNotNull null
-            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
-            newMovieSearchResponse(title, fixUrl(href), TvType.Movie)
-        }
+        return emptyList()
     }
 }
