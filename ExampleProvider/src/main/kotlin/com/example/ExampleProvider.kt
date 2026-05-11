ab05f0aec58e2f5b4a782b0c2929ab65ccaf9475
bikbakboo <ben1641979@gmail.com>
2026-05-11 20:42:59 +0200

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,21 +1,26 @@
 package com.example
 
-import com.lagradost.cloudstream3.MainAPI
-import com.lagradost.cloudstream3.SearchResponse
-import com.lagradost.cloudstream3.TvType
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
 
-class ExampleProvider : MainAPI() { // All providers must be an instance of MainAPI
-    override var mainUrl = "https://example.com/" 
-    override var name = "Example provider"
-    override val supportedTypes = setOf(TvType.Movie)
+class FilmpalastProvider : MainAPI() {
+    override var mainUrl = "https://filmpalast.to"
+    override var name = "Filmpalast"
+    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
 
-    override var lang = "en"
-
-    // Enable this when your provider has a main page
-    override val hasMainPage = true
-
-    // This function gets called when you search for something
     override suspend fun search(query: String): List<SearchResponse> {
-        return listOf()
+        val url = "$mainUrl/search/story/$query"
+        val response = app.get(url).text
+        val document = org.jsoup.Jsoup.parse(response)
+
+        return document.select("article.item").mapNotNull {
+            val title = it.selectFirst("h2")?.text() ?: return@mapNotNull null
+            val href = it.selectFirst("a")?.attr("href") ?: return@mapNotNull null
+            val poster = it.selectFirst("img")?.attr("src")
+            
+            newMovieSearchResponse(title, fixUrl(href), TvType.Movie) {
+                this.posterUrl = fixUrl(poster ?: "")
+            }
+        }
     }
 }
